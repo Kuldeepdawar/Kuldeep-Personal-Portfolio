6e7bb2047a9b2e2d2e71c5fb1f9824d38b752342
KuldeepDawar <kuldeepdawar47@gmail.com>
2025-04-30 08:42:36 +1000

    updated resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,7 +97,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/React Full Stack Developer.docx"
+          href="/Kudeep_React_Developer.docx"
           download
         >
           Download CV{" "}
